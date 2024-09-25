6cfc88a7ab76d1654f36cdc97118463bf4892410
KrishnasGIT <39901504+KrishnasGIT@users.noreply.github.com>
2024-09-25 18:49:42 +0530

    proper calls

--- src/main/java/com/qa/OrderManagement/pages/PatientUserHomePage.java
+++ src/main/java/com/qa/OrderManagement/pages/PatientUserHomePage.java
@@ -1,21 +1,61 @@
 package com.qa.OrderManagement.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.qa.OrderManagement.base.TestBase;
 
-public class PatientUserHomePage extends TestBase{
+public class PatientUserHomePage extends TestBase
+{
 	
     public PatientUserHomePage() {
     	PageFactory.initElements(driver, this);
     	
     }
-	
-	WebElement PaymentReminderPrmpt=driver.findElement(By.xpath("/html/body/div[7]/div[3]/div"));
-	WebElement ClosePaymentReminderBtn=driver.findElement(By.xpath("/html/body/div[7]/div[3]/div/div[2]/button"));
-	WebElement NewDocumentRequest=driver.findElement(By.xpath("/html/body/div[1]/div/div/div[1]/div[2]/div/div[1]/nav/div[1]"));
+    
+    @FindBy(xpath="//a[@class='flex items-center h-full w-full' and @href='/documentsRequest']")
+    WebElement newDocumentRequest;
+    
+    @FindBy(xpath="//p[text()='Doctor Facility']")
+    WebElement DRSelecTypeasDF;
+    
+    @FindBy(xpath="//p[text()='Attorney Office']")
+    WebElement DRSelecTypeasAO;
+    
+    @FindBy(xpath="//div[contains(@class,' css-1wy0on6')]")
+    WebElement DFList;
+    
+    @FindBy(xpath="//div[contains(@class,'select__indicators css-1wy0on6')]")
+    WebElement RequestTypeList;
+    
+    @FindBy(xpath="//span[contains(@class,'text-sm font-semibold text-white') and starts-with(text(),'Medical')]")
+    WebElement RecordTypeMedical;
+    
+    @FindBy(xpath="//span[contains(@class,'text-sm font-semibold text-white') and starts-with(text(),'Billing')]")
+    WebElement RecordTypeBilling;
+    
+    @FindBy(xpath="//span[contains(@class,'text-sm font-semibold text-white') and starts-with(text(),'X-Ray')]")
+    WebElement RecordTypeXray;
+    
+    @FindBy(xpath="//h6[text()='Passport']")
+    WebElement PassportID;
+    
+    @FindBy(xpath="(//div/div/input[@type='file'])[1]")
+    WebElement PassportFpage;
+    //(//input[contains(@class,'upload-input draggable') and @type='file' ])[1]
+    
+    
+	/*
+	 * WebElement PaymentReminderPrmpt=driver.findElement(By.xpath(
+	 * "/html/body/div[7]/div[3]/div")); WebElement
+	 * ClosePaymentReminderBtn=driver.findElement(By.xpath(
+	 * "/html/body/div[7]/div[3]/div/div[2]/button")); WebElement
+	 * NewDocumentRequest=driver.findElement(By.xpath(
+	 * "/html/body/div[1]/div/div/div[1]/div[2]/div/div[1]/nav/div[1]"));
+	 */
 	
 	//@FindBy()
 	/*
@@ -55,20 +95,61 @@ public class PatientUserHomePage extends TestBase{
 
     
     //Actions
-    public void paymentRemainder()
-    {
-        PaymentReminderPrmpt.click();
-    	ClosePaymentReminderBtn.click();    
+	/*
+	 * public void paymentRemainder() { PaymentReminderPrmpt.click();
+	 * ClosePaymentReminderBtn.click();
+	 */  
    
-}
- 
     public void newdocumentRequest() 
     {
-    	NewDocumentRequest.click();
+    	newDocumentRequest.click();
     }
     
-    public void selectType()
+    public void selectDType(String type)
     {
-    	//SelectTypeAttorneyOffice.click();
+    	if(type.equalsIgnoreCase("DF"))
+    	{
+    	DRSelecTypeasDF.click();
+    	}else if(type.equalsIgnoreCase("AO")) {
+    		DRSelecTypeasAO.click();
+    	}
+    		
     }
+public void ItemListclick() {
+	DFList.click();
+}
+
+public void requestTypeList() 
+{
+	RequestTypeList.click();
+
+}
+
+public void selectRecordType(String rtype )
+{
+	if(rtype.equalsIgnoreCase("Medical"))
+	{
+	RecordTypeMedical.click();
+}else if(rtype.equalsIgnoreCase("Billing"))
+{
+	RecordTypeBilling.click();
+}else if(rtype.equalsIgnoreCase("XRAY"))
+{
+	RecordTypeXray.click();
+}
+
+}
+public void selectIdentity() {
+	PassportID.click();
+	
+}
+
+public void clickonuploadpassportFpage()
+{
+	JavascriptExecutor js = (JavascriptExecutor)driver;
+	js.executeScript("arguments[0].click()", PassportFpage);
+	//PassportFpage.click();
+
+}
+
 }
--- src/test/java/com/qa/Ordermanagement/Test/PatientUserOrderTest.java
+++ src/test/java/com/qa/Ordermanagement/Test/PatientUserOrderTest.java
@@ -1,6 +1,13 @@
 package com.qa.Ordermanagement.Test;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -9,10 +16,12 @@ import org.testng.annotations.Test;
 import com.qa.OrderManagement.base.TestBase;
 import com.qa.OrderManagement.pages.LoginPage;
 import com.qa.OrderManagement.pages.PatientUserHomePage;
+import com.qa.OrderManagement.utilities.Utilities;
 
 public class PatientUserOrderTest extends TestBase {
 	//LoginPage loginpage;
 	//PatientUserHomePage puhomepage;
+	 
 
 	public PatientUserOrderTest() {
 		super();
@@ -30,26 +39,49 @@ public class PatientUserOrderTest extends TestBase {
 	@Test(priority = 1)
 	public void patientuserOrderCreationTest() throws Throwable {
 		LoginPage loginpage = new LoginPage(driver);
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		String fName2upload="C:\\Users\\krish\\OneDrive\\Documents\\Vantashala\\3sep.pdf";
 
 		// System.out.println("from test method");
 		//extentTest = extentReports.createTest("loginPageLogoTest", "View Test Result");
 		loginpage.login(properties.getProperty("patientuser"), properties.getProperty("Password"));
 		Thread.sleep(2000);
 		// puhomepage = new PatientUserHomePage();
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		PatientUserHomePage puhomepage = new PatientUserHomePage();
-		puhomepage.paymentRemainder();
-		Thread.sleep(3000);
-		System.out.print("reminderResult");
+		//puhomepage.paymentRemainder();
+		//driver.findElement(By.xpath(String.format("//p[normalize-space()='%s']",properties.getProperty("doctorFacility1")))).click();
+		
+		//Thread.sleep(3000);
+		//System.out.print("reminderResult");
 		//extentTest = extentReports.createTest("reminderResult", "View Test Result");
 		Thread.sleep(2000);
 		puhomepage.newdocumentRequest();
+		puhomepage.selectDType("DF");
 		//extentTest = extentReports.createTest("newdocumentRequest click", "View Test Result");
 		Thread.sleep(2000);
-		/*
-		 * puhomepage.selectType(); extentTest =
-		 * extentReports.createTest("newdocumentrequestTest", "View Test Result");
-		 */
+		puhomepage.ItemListclick();
+		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(1000);
+		driver.findElement(By.xpath(String.format("//p[normalize-space()='%s']",properties.getProperty("doctorFacility1")))).click();
+		puhomepage.requestTypeList();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath(String.format("//span[normalize-space()='%s']",properties.getProperty("RequestType")))).click();
+		Thread.sleep(1000);
+		js.executeScript("window.scrollBy(0,500)");
+		puhomepage.selectRecordType("Medical");
+		Thread.sleep(1000);
+		puhomepage.selectIdentity();
+		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,500)");
+		Thread.sleep(2000);
+		puhomepage.clickonuploadpassportFpage();
+		Thread.sleep(2000);
+		//Utilities util = new Utilities();
+		fileupload(fName2upload);
+		Thread.sleep(2000);
+
 	}
 
 	/*
@@ -61,8 +93,36 @@ public class PatientUserOrderTest extends TestBase {
 	 * 
 	 * }
 	 */
+	
+	public void fileupload(String fname) throws AWTException
+	{
+		Robot r = new Robot();
+		r.setAutoDelay(1000);
+		
+		StringSelection ss = new StringSelection(fname);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+		r.setAutoDelay(2000);
+		
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_V);
+		
+		r.keyRelease(KeyEvent.VK_CONTROL);
+		r.keyRelease(KeyEvent.VK_V);
+		
+		r.setAutoDelay(2000);
+		
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		
+		r.setAutoDelay(2000);
+		js.executeScript("window.scrollBy(0,100)");
+		
+		//driver.manage().window().minimize();
+		//driver.manage().window().maximize();
+		
+	}
 	 @AfterClass 
 	  public void closebrowsers() 
-	  { driver.quit(); 
+	  { //driver.quit(); 
 	  }
 }
